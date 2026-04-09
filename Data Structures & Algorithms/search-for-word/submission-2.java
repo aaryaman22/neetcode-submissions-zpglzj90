275f7841a4f25dacbfe1e90fc799148958c8522d
aaryaman <78210804+aaryaman22@users.noreply.github.com>
2026-04-09 22:43:35 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                if(wordMade(board, word, i , j , 0))return true;
+            }
+        }
+        return false;
+    }
+    private boolean wordMade(char[][] board, String word, int i , int j, int len){
+        if(word.length() == len)return true;
+        if(i >= board.length || j >= board[0].length || i < 0 || j < 0 || board[i][j] != word.charAt(len)){
+            return false;
+        }
+        char c = board[i][j];
+        board[i][j] = '*';
+        boolean fnd =  wordMade(board, word,  i+1 ,  j,  len+1) ||
+                wordMade(board, word,  i-1 ,  j,  len+1) ||
+                wordMade(board, word,  i ,  j+1,  len+1) ||
+                wordMade(board, word,  i ,  j-1,  len+1);
+        board[i][j] = c;
+        return fnd;
+    }
+}
